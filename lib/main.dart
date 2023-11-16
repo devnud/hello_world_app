41265667e3faa89557af84f5208bf5ec44c51846
devnud <taximadridavid@gmail.com>
2023-11-16 12:15:22 +0100

    añadiendo Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,13 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: Text('Hola Mundo'),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child: Text('Hola Mundo'),
+        ),
       ),
     );
   }
